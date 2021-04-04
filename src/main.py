b0e4351df4720a460bf7cb6498f672cac6943060
YK0L0DIY <ykolodiy17@gmail.com>
2021-04-04 17:52:39 +0100

    new image

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from src.utils.path import get_download_path
 
 if __name__ == '__main__':
     path = download_image(
-        "https://images.unsplash.com/photo-1597177285888-1d26937a29da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        "https://images.unsplash.com/34/BA1yLjNnQCI1yisIZGEi_2013-07-16_1922_IMG_9873.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2702&q=80",
         get_download_path(),
         f"{int(datetime.now().timestamp())}.jpg")
     set_wallpaper(path)
